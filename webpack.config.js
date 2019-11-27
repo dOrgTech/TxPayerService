e4413e5e3b811a04082d53cecacd9106dea849f2
dOrgJelli <jelli@dorg.tech>
2019-11-26 22:37:28 -0500

    build works, but running + testing is broken

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js"
+  },
+  node: {
+    fs: "empty",
+    net: "empty"
+  }
+};
