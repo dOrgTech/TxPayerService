087e29b09296a8523cffe012d5e29e762c0cc5ff
Flash <flash.jaq@gmail.com>
2019-12-14 20:05:25 +0100

    Added email sending for gmail

--- src/api/email/index.js
+++ src/api/email/index.js
@@ -1,9 +1,32 @@
+import nodemailer from "nodemailer";
+const { BALANCE_NOTIFICATIONS_EMAIL } = process.env;
+
+// Tested with personal gmail
+const transporter = nodemailer.createTransport({
+  pool: true,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "",
+    pass: ""
+  }
+});
+
+const message = (balance, wallet) => ({
+  from: "TxPayerService <DOrg Bot>",
+  to: `DOrg Funding<${BALANCE_NOTIFICATIONS_EMAIL}>`,
+  subject: "LOW BALANCE ALERT - TX_PAYER_SERVICE",
+  text: `Ethereum wallet: ${wallet}
+  Wallet balance: ${balance}`,
+  html: `<h2>Tx Payer Service</h2><p>Ethereum wallet: <a href=https://etherscan.io/address/${wallet}>${wallet}</a></p><p>Wallet balance: ${balance} ETH</p>`
+});
+
 export const handleFunds = async (_, response, next) => {
   const { balance, wallet } = response.locals;
   if (balance && wallet) {
-    console.log(`sending email has been sent to refill wallet ${wallet}`);
+    transporter.sendMail(message(balance, wallet));
     return;
   }
-  //console.log("not sending email");
   next();
 };
