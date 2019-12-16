19cadb3a1d6bbc7fe053d86c645f42d189ac911e
Flash <flash.jaq@gmail.com>
2019-12-16 19:52:20 +0100

    Added logging and error handling

--- src/api/email/index.js
+++ src/api/email/index.js
@@ -10,18 +10,6 @@ const {
   EMAIL_PASSWORD
 } = process.env;
 
-// Tested with personal gmail
-const transporter = nodemailer.createTransport({
-  // pool: true,
-  host: EMAIL_HOST,
-  port: EMAIL_PORT,
-  // secure: true,
-  auth: {
-    user: EMAIL_USER,
-    pass: EMAIL_PASSWORD
-  }
-});
-
 const message = (balance, wallet, cancelled) => ({
   from: `TxPayerService <${EMAIL}>`,
   to: `DOrg Funding <${BALANCE_NOTIFICATIONS_EMAIL}>`,
@@ -30,11 +18,37 @@ const message = (balance, wallet, cancelled) => ({
   html: `<h2>Tx Payer Service</h2><p>Ethereum wallet: <a href=https://etherscan.io/address/${wallet}>${wallet}</a></p><p>Wallet balance: ${balance} ETH</p>`
 });
 
+const sendEmail = (balance, wallet, cancelled) => {
+  const transporter = nodemailer.createTransport({
+    // pool: true,
+    host: EMAIL_HOST,
+    port: EMAIL_PORT,
+    // secure: true,
+    auth: {
+      user: EMAIL_USER,
+      pass: EMAIL_PASSWORD
+    }
+  });
+
+  transporter.sendMail(message(balance, wallet, cancelled), (err, res) => {
+    if (err) {
+      console.log(
+        `Failed to send ${
+          cancelled ? "critical" : "low"
+        } balance email:\n${err}`
+      );
+    } else {
+      console.log(`Sent ${cancelled ? "critical" : "low"} balance email`);
+    }
+  });
+};
+
 export const handleFunds = async (_, response, next) => {
   const { balance, wallet, cancelled } = response.locals;
 
-  if (BALANCE_NOTIFICATIONS_EMAIL && balance < BALANCE_NOTIFICATION_THRESHOLD)
-    transporter.sendMail(message(balance, wallet, cancelled));
+  if (BALANCE_NOTIFICATIONS_EMAIL && balance < BALANCE_NOTIFICATION_THRESHOLD) {
+    sendEmail(balance, wallet, cancelled);
+  }
 
   if (!cancelled) next();
 };
